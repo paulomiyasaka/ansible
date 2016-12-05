e86446d914365a691e49db06dfbd3c46739a5eb0
paulomiyasaka <paulomiyasaka@gmail.com>
2016-12-05 14:14:42 -0200

    Create init.php

---
+++ aplicacao/init.php
@@ -0,0 +1,18 @@
+<?php
+  
+// constantes com as credenciais de acesso ao banco MySQL
+define('DB_HOST', '192.168.0.34');
+define('DB_USER', 'aplicacao');
+define('DB_PASS', '12345');
+define('DB_NAME', 'aula_dirceu');
+  
+// habilita todas as exibições de erros
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+ 
+date_default_timezone_set('America/Sao_Paulo');
+  
+// inclui o arquivo de funçõees
+require_once 'functions.php';
+
+?>
