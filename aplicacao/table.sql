a24210a10b206cc805a81fc4c07547882c7a7c42
paulomiyasaka <paulomiyasaka@gmail.com>
2016-12-05 14:15:08 -0200

    Create table.sql

---
+++ aplicacao/table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE aula_dirceu;
+USE aula_dirceu;
+
+CREATE TABLE users(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- id
+    name VARCHAR(60) NOT NULL, -- nome
+    email VARCHAR(80) NOT NULL, -- email
+    gender ENUM('m', 'f') NOT NULL, -- gênero (masculino, feminino)
+    birthdate DATE NOT NULL, -- data de nascimento
+    PRIMARY KEY(id)
+) COLLATE=utf8_unicode_ci;
